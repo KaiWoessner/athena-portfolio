c98755d1b735c9c797ef72406bca09734aaffa5a
KaiWoessner <kainoawoessner@gmail.com>
2026-01-06 11:49:34 -0800

    Updated position of arrows and centered vertical padding for gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -15,31 +15,31 @@ function Gallery() {
     setIndex((index - 1 + images.length) % images.length);
 
   return (
-    <section className="bg-white px-8 py-6 text-center">
+    <section className="bg-white my-8 px-8 text-center">
       <div className="relative mx-auto">
-        <div className="w-full max-w-full sm:max-w-3xl md:max-w-4xl flex items-center justify-center bg-white-300 rounded shadow-lg aspect-[4/3] mx-auto">
+        <div className="relative w-full max-w-full sm:max-w-3xl md:max-w-4xl aspect-[4/3] overflow-hidden mx-auto flex items-center justify-center">
           <img
             src={images[index]}
             alt="Performance"
-            className="max-w-full max-h-full object-contain"
+            className="block max-w-full max-h-full object-contain"
           />
+
+          <button
+            onClick={prev}
+            className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 px-3 py-2 rounded shadow border-2 border-black cursor-pointer"
+          >
+            ‹
+          </button>
+
+          <button
+            onClick={next}
+            className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 px-3 py-2 rounded shadow border-2 border-black cursor-pointer"
+          >
+            ›
+          </button>
         </div>
-
-        {/* Controls */}
-        <button
-          onClick={prev}
-          className="absolute left-0 top-1/2 -translate-y-1/2 bg-white/80 px-3 py-2 rounded shadow border-2 border-black cursor-pointer"
-        >
-          ‹
-        </button>
-
-        <button
-          onClick={next}
-          className="absolute right-0 top-1/2 -translate-y-1/2 bg-white/80 px-3 py-2 rounded shadow border-2 border-black cursor-pointer"
-        >
-          ›
-        </button>
       </div>
+
     </section>
   );
 }
